a42b0e035f4369895bfae1aa98e4eaea2cdadf4e
Ilya Dragonromeo <you@example.com>
2023-10-22 18:30:48 +0300

    fix: key going in localStorage after click btn

--- src/components/MainPage/Search/Search.tsx
+++ src/components/MainPage/Search/Search.tsx
@@ -9,18 +9,24 @@ type State = {
 };
 
 export default class Search extends Component<Props, State> {
+  inputValue: string;
   constructor(props: Props | Readonly<Props>) {
     super(props);
     this.state = {
       value: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.inputValue = '';
   }
 
   handleChange(event: { target: { value: string } }) {
     this.setState({ value: event.target.value });
-    //TODO: localStorage.Set should use after click btn 'search' - not like that.
-    localStorage.setItem('inputKey', event.target.value);
+    this.inputValue = event.target.value;
+  }
+
+  handleClick() {
+    localStorage.setItem('inputKey', this.inputValue);
   }
 
   //TODO: add placeholder
@@ -34,7 +40,7 @@ export default class Search extends Component<Props, State> {
           value={this.state.value}
           onChange={this.handleChange}
         />
-        <button>search</button>
+        <button onClick={this.handleClick}>search</button>
       </div>
     );
   }
