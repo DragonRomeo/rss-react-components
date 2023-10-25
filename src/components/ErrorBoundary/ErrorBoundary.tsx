623890680f416c95b4d389408f8c5bf3e0e66f81
Ilya Dragonromeo <you@example.com>
2023-10-25 21:52:50 +0300

    refactor: remove extra coments & code from ErrorBo

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,7 +1,6 @@
 import { Component, ErrorInfo } from 'react';
 import MainPage from '../MainPage/MainPage';
 import ErrorBtn from './ErrorBtn/ErrorBtn';
-// ErrorBtn
 
 type Props = {
   children?: JSX.Element;
@@ -20,18 +19,11 @@ export class ErrorBoundary extends Component<Props, State> {
   static getDerivedStateFromError(error: Error) {
     console.log(error);
     console.log('поймал ошибку');
-    // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
     return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.log(error, errorInfo);
-    console.log('поймал ошибку');
   }
 
   render() {
     if (this.state.hasError) {
-      // Можно отрендерить запасной UI произвольного вида
       return <h1>Ну вот, ты все выключил(а), вселенную считай сломал(а)</h1>;
     }
 
