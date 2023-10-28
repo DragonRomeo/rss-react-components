6719f855ba419f6104d47f4d78814ada5e973bf1
Ilya Dragonromeo <you@example.com>
2023-10-29 00:01:58 +0300

    fix: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://DragonRomeo.github.io/react-components/',
+  base: 'https://DragonRomeo.github.io/rss-react-components/',
   plugins: [react()],
 });
