d581b47b0d6311fe7c1fca904ac1c9c80fe6ddde
Ilya Dragonromeo <you@example.com>
2023-11-01 14:33:10 +0300

    fix: remove extra com, rewrite app on class comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Component } from 'react';
 import './assets/styles/global.css';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
-// import MainPage from './components/MainPage/MainPage';
 
-export const App = () => {
-  return (
-    <>
-      <ErrorBoundary />
-    </>
-  );
-};
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <ErrorBoundary />
+      </>
+    );
+  }
+}
