c2f4b9377340b9d67eac96a7e21d0357835f698e
Ilya Dragonromeo <you@example.com>
2023-11-05 22:05:58 +0300

    fix: fix number of Pagination pages

--- src/components/MainPage/Pagination/Pagination.tsx
+++ src/components/MainPage/Pagination/Pagination.tsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Context } from '../../../providers/context';
 
 export const Pagination = ({ updateData }) => {
   const navigate = useNavigate();
+  const maxItems = 100;
+  const { perPage } = useContext(Context);
+  const numberOfPage = maxItems / +perPage;
+  const arrPage = new Array(numberOfPage).fill(0);
 
   const handleClick = (event) => {
     const eValue = event.target.innerText;
@@ -12,9 +18,15 @@ export const Pagination = ({ updateData }) => {
 
   //TODO: кол-во кнопок должно зависеть от того, сколько в итоге получится страниц.
   //TODO: общее кол-во элементов / кол-во айтемов на странице = кол-во кнопок
+
   return (
     <div className="pagination">
-      <button className="pageBtn" onClick={(e) => handleClick(e)}>
+      {arrPage.map((item, index) => (
+        <button key={index} className="pageBtn" onClick={(e) => handleClick(e)}>
+          {index + 1}
+        </button>
+      ))}
+      {/* <button className="pageBtn" onClick={(e) => handleClick(e)}>
         1
       </button>
       <button className="pageBtn" onClick={(e) => handleClick(e)}>
@@ -25,7 +37,7 @@ export const Pagination = ({ updateData }) => {
       </button>
       <button className="pageBtn" onClick={(e) => handleClick(e)}>
         4
-      </button>
+      </button> */}
     </div>
   );
 };
