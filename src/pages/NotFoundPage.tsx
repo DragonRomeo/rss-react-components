cbadad60aace215a1b76f1dff008989667faf090
Ilya Dragonromeo <you@example.com>
2023-11-05 21:10:32 +0300

    feat: add error page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+export const NotFoundPage = () => {
+  return <div>Error!!!!!!!!! Nothing found!!!!!</div>;
+};
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -2,6 +2,7 @@ import { ErrorBoundary } from '../components/ErrorBoundary/ErrorBoundary';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
 import { Results } from '../components/MainPage/Results/Results';
+import { NotFoundPage } from '../pages/NotFoundPage';
 
 export const App = () => {
   return (
@@ -11,6 +12,8 @@ export const App = () => {
           <Route index element={<HomePage />}></Route>
 
           <Route path="/rss-react-components/results" element={<Results />}></Route>
+
+          <Route path="/rss-react-components*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </>
